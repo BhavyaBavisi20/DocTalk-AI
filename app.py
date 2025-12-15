692fd6ee44d97cc13f0d76fe5664b6a6dc343776
BhavyaBavisi20 <bhavyabavisi40@gmail.com>
2025-12-16 00:29:31 +0530

    Add RecursiveCharacterTextSplitter import

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pptx import Presentation
 import openpyxl
 from io import BytesIO
 from langchain_community.vectorstores import FAISS
+from langchain.text_splitter.recursive import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document as LCDocument
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from gtts import gTTS
